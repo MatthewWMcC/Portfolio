c7d3a3b92b2dcd7228a8669091a08e58459bffc0
Matthew McCracken <matthewwmccracken@gmail.com>
2023-12-14 19:36:04 -0500

    resume update

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <chakra.iframe
           height={"100%"}
           width="100%"
-          src="https://drive.google.com/file/d/1F2lTd3Aq2ECcnhi07Sl3jFcFLYmMgZZg/preview?usp=sharing"
+          src="https://drive.google.com/file/d/16o_sq1oC1ciTSg78loNWy6sdJqGUvfvR/preview?usp=sharing"
         ></chakra.iframe>
       </Box>
     </>
